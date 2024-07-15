ad6479471bea5e3dcd06aef892d30d659e2160ff
Caio Lima <lima.caioalves@gmail.com>
2024-07-14 21:17:58 -0300

    new file:   JavaScript/functions.js new file:   JavaScript/functions_2.js modified:   JavaScript/index.html

---
+++ JavaScript/functions.js
@@ -0,0 +1,10 @@
+// Criar um aplicativo de frases motivacionais
+
+//Declaração da funcção (Function Statement)
+function createPhrases () {
+  console.log('Estudar é muito bom');
+  console.log('Paciência e persistência');
+  console.log('Revisão é mae do aprendizado');
+};
+
+createPhrases();
