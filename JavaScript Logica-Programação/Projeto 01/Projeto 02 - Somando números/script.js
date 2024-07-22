b220e3894504a5761ec10ad0b8d7c2991df32488
Caio Lima <lima.caioalves@gmail.com>
2024-07-22 06:43:00 -0300

    s

---
+++ JavaScript Logica-Programação/Projeto 01/Projeto 02 - Somando números/script.js
@@ -0,0 +1,9 @@
+/*
+  Type Casting (Conversão de tupos)
+*/
+let numberOne = prompt('Digite o primeiro número: ')
+let numberTwo = prompt("Digite o segundo número: ")
+
+let total = (Number(numberOne) + Number(numberTwo)) / 2
+
+alert(total)
--- JavaScript Logica-Programação/Projeto 01/script.js
+++ JavaScript Logica-Programação/Projeto 01/script.js
@@ -1,1 +1,14 @@
-alert('Ola Mundo!')
+/*
+
+Encontre a solução ao problema
+
+Pergunte o nome do usuário e escreva a mensagem:
+"Ola, [nome do usuário]"
+
+*/
+let UserName = prompt("Qual o seu nome?")
+
+alert("Ola, " + UserName)
+
+//console.log(alert(`Olá ${UserName}`))
+
