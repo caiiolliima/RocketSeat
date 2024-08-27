fb46b3679b6dae1f0abf69773816d54edda02c4d
Caio Lima <lima.caioalves@gmail.com>
2024-08-27 06:43:23 -0300

    Commit

---
+++ styled-components/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "./Components/Button";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./Styles/Themes/default";
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary"/>
+      <Button variant= "secondary"/>
+      <Button variant= "danger"/>
+      <Button variant= "success"/>
+      <Button  />
+    </ThemeProvider>
+  )
+}
