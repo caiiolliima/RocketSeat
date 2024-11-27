886038551523676740837e7f67901947b79ff5c0
Caio Lima <lima.caioalves@gmail.com>
2024-11-27 05:48:23 -0300

    Resolução do problema dos métodos nas classes

--- Python/Módulo 02/Projeto Módulo 02/jogo.py
+++ Python/Módulo 02/Projeto Módulo 02/jogo.py
@@ -8,28 +8,29 @@ class Personagem:
     self.__vida = vida
     self.__nivel = nivel
 
-    def get_nome(self):
-      return self.__nome
+  def get_nome(self):
+    return self.__nome
     
-    def get_vida(self):
-      return self.__vida
+  def get_vida(self):
+    return self.__vida
     
-    def get_nivel(self):
-      return self.__nivel
+  def get_nivel(self):
+    return self.__nivel
     
-    def exibir_detalhes(self):
-      return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
+  def exibir_detalhes(self):
+    return f"Nome: {self.get_nome()}\nVida: {self.get_vida()}\nNível: {self.get_nivel()}"
     
 class Heroi(Personagem):
   def __init__(self, nome, vida, nivel, habilidade):
     super().__init__(nome, vida, nivel)
     self.__habilidade = habilidade
 
-    def get_habilidade(self):
-      return self.__habilidade
+  def get_habilidade(self):
+    return self.__habilidade
     
-    def exibir_detalhes(self):
-      return f"{super().exibir_detalhes()}\n Habilidade: {self.get_habilidade()}\n"
+  
+  def exibir_detalhes(self):
+    return f"{super().exibir_detalhes()}\n Habilidade: {self.get_habilidade()}\n"
     
 
 class Inimigo(Personagem):
@@ -37,11 +38,11 @@ class Inimigo(Personagem):
     super().__init__(nome, vida, nivel)
     self.__tipo = tipo
 
-    def get_tipo(self):
-      return self.__tipo
+  def get_tipo(self):
+    return self.__tipo
     
-    def exibir_detalhes(self):
-      return f"{super().exibir_detalhes()}\n Tipo: {self.get_tipo()}\n"
+  def exibir_detalhes(self):
+    return f"{super().exibir_detalhes()}\n Tipo: {self.get_tipo()}\n"
     
 heroi = Heroi("Heroi", 100, 5, "Super-Força")
 
