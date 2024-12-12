54009649dbbc2b2d531834bc197df2c08e908abd
CaioLimaEK <caio.lima@elringklinger.com>
2024-12-12 16:07:01 -0300

    Commit conection to sqlite in app

--- Python/Nivel 2/Sample Flask Auth/app.py
+++ Python/Nivel 2/Sample Flask Auth/app.py
@@ -3,12 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config ['SECRET_KEY'] = "your_scret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 dn = SQLAlchemy(app)
 
 @app.route("/hello-world", methods=['GET'])
 def hello_world():
     return "Hello world"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
