4bb5c590610243f7201fb6c533649749163da0bd
CaioLimaEK <caio.lima@elringklinger.com>
2025-01-02 15:57:48 -0300

    Feat: Implement users roles and create MySQL connection

--- Python/Nivel 2/app.py
+++ Python/Nivel 2/app.py
@@ -1,79 +1,99 @@
 from flask import Flask, request, jsonify
-from models.tasks import task
-#__name__ = "main"
+from database import db
+from models.user import User
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
-#Tabela tarefa
+login_mangager = LoginManager()
+db.init_app(app)
+login_mangager.init_app(app)
 
-tasks = []
+#view loginex
+login_mangager.login_view = 'login'
 
-task_id_control = 1
+@login_mangager.user_loader
+def load_user(user_id):
+     return User.query.get(user_id)
 
-@app.route('/tasks', methods=['POST'])
-def create_task():
-    global task_id_control
-    data = request.get_json()
-    new_task = task(task_id_control,data.get("title"),data.get("description", ""))
-    task_id_control += 1
-    tasks.append(new_task)
-    print(tasks)
-    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
+@app.route("/login", methods=["POST"])
+def login():
+    data=request.json
+    username=data.get("username")
+    password=data.get("password")
 
-@app.route('/tasks', methods=['GET'])
-def get_tasks():
-    task_list = [task.to_dict() for task in tasks]
-    # for task in tasks:
-    #     task_list.append(task.to_dict())
-    output = {
-              "tasks": task_list,
-              "total_tasks": len(task_list)
-            }
-    return jsonify(output)
+    if username and password:
+        
+        user = User.query.filter_by(username=username).first()
 
-@app.route('/tasks/<int:id>', methods=['GET'])
-def get_task(id):
-    for t in tasks:
-        if t.id == id:
-            return jsonify(t.to_dict())
-    return jsonify({"message": "Não foi possível encontrar a atividade!"}), 404
+        if user and user.password == password:
+                login_user(user)
+                print(current_user.is_authenticated)
+                return jsonify({"message": "Autenticação realizada com sucesso"})
+    
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
-@app.route('/tasks/<int:id>', methods=['PUT'])
-def update_task(id):
-    task = None
-    for t in tasks:
-        if t.id == id:
-            task = t
-            break
+@app.route("/logout", methods=['GET'])
+@login_required
+def logout():
+     logout_user()
+     return jsonify({"message": "Logout realizado com sucesso"})
 
-    print(task)
-    if task == None:
-        return jsonify({"message": "Não foi possível encontrar a atividade!"}), 404
+@app.route("/user", methods=['POST'])
+@login_required #Usar quando o sistema for fechado, ou seja, somente usuários cadastrados podem criar novos usuários.
+def create_user():
+     data = request.json
+     username = data.get("username")
+     password = data.get("password")
 
-    data = request.get_json()
+     if username and password:
+          user = User(username=username, password=password)
+          db.session.add(user)
+          db.session.commit()
+          return jsonify({"message": "Usuário cadastrado com sucesso"})
+     
+     return jsonify({"message": "Credenciais inválidas"}), 400
 
-    task.title = data['title']
-    task.description = data['description']
-    task.completed = data['completed']
+@app.route("/user/<int:id_user>", methods={'GET'})
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
 
-    print(task)
-    return jsonify({"message": "Tarefa atualizada com sucesso"})
+    if user:
+        return {"username": user.username}
+    
+    return jsonify({"message": "Usuário não encontrado"}), 404
 
-@app.route('/tasks/<int:id>', methods=['DELETE'])
-def delete_task(id):
-    task = None
-    for t in tasks:
-        print(t.to_dict())
-        if t.id == id:
-            task = t
-            break
+@app.route("/user/<int:id_user>", methods={'PUT'})
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
 
-    if not task:
-        return jsonify({"message": "Não foi possível encontrar a atividade!"}), 404
+    if user and data.get("password"):
+         user.password = data.get("password")
+         db.session.commit()
 
-    tasks.remove(task)
+         return jsonify ({"message": f"Usuário {id_user} alterado com sucesso"})
+    
+    return jsonify ({"message": "Usuário não encontrado"}), 404
 
-    return jsonify({"message": "Tarefa deletada com sucesso"})
+@app.route("/user/<int:id_user>", methods=['DELETE'])
+@login_required
+def delete_user(id_user):
+     user = User.query.get(id_user)
 
-if __name__ == "__main__":
+     if user == current_user.id:
+          return jsonify ({"message": "Deleção não permitida"}), 403
+
+     if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify ({"message": f"Usuário {id_user} deletado com sucesso"})
+     
+     return jsonify ({"message": "Usuário não encontrdo"}), 404
+
+if __name__ == '__main__':
     app.run(debug=True)
-
