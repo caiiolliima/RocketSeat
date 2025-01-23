2e49f5d59a07012d8a908d975b375d2501a4e3f0
CaioLimaEK <caio.lima@elringklinger.com>
2025-01-23 16:42:24 -0300

    Commit API PIX

--- Python/Nivel 3/app.py
+++ Python/Nivel 3/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
+from payments.pix import Pix
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -20,12 +21,21 @@ def create_payment_pix():
     expiration_date = datetime.now() + timedelta(minutes=30)
 
     new_payment = Payment(value=data['value'], expiration_date=expiration_date)
-
+    
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+    new_payment.qr_code = data_payment_pix["qr_code_path"]
+    
     db.session.add(new_payment)
-    db.session.commi()
+    db.session.commit()
 
-    return jsonify ({"message" : "O pagamento foi criado"},
-                    {"payment" : ""})
+    return jsonify ({"message" : "O pagamento foi criado",
+                    "payment" : new_payment.to_dict()})
+    
+@app.route("/pagamento/pix/qr_code/<file_name>", methods=['GET'])
+def get_image(file_name):
+    return send_file(f"static/img/{file_name}.png", mimetype='image/png')
 
 @app.route("/pagamento/pix/confirmation", methods=['POST'])
 def pix_confirmation():
