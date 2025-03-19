ca9c4c5e1ccb818b7f75e430db47ba54670df71a
Caio Lima <lima.caioalves@gmail.com>
2025-03-19 19:36:47 -0300

    commit

--- Python/stocks/stocks.py
+++ Python/stocks/stocks.py
@@ -1,28 +1,13 @@
 import yfinance as yf
 import pandas as pd
-import os
+import backtrader as bt
+import matplotlib as plt
 
-# Definir o caminho correto do arquivo
-file_path = "stocks/acoes.xlsx"
+stock = "INTC"
+start_date = "2025-03-06"
+end_date = "2025-03-07"
 
-# Verificar se o arquivo existe
-if not os.path.exists(file_path):
-    print(f"Erro: O arquivo '{file_path}' não foi encontrado!")
-    exit()  # Para o script se o arquivo não existir
 
-# Carregar planilha
-df = pd.read_excel(file_path)
+data = yf.download(stock, start=start_date, end=end_date)
 
-# Atualizar preços
-for index, row in df.iterrows():
-    ticker = row["Ticker"]
-    stock = yf.Ticker(ticker)
-    hist = stock.history(period="2d")  # Pega os últimos 2 dias
-    
-    if not hist.empty:
-        df.at[index, "Fechamento Anterior"] = hist["Close"].iloc[-2]  # Fechamento do dia anterior
-
-# Salvar de volta no Excel
-df.to_excel(file_path, index=False)
-
-print("Planilha atualizada com sucesso!")
+print(data.head())
