f7f14490864ecd93930d44de4679bf0712211707
CaioLimaEK <caio.lima@elringklinger.com>
2025-03-23 20:11:14 -0300

    commit slug

--- typeScript/src/domain/entities/question.ts
+++ typeScript/src/domain/entities/question.ts
@@ -1,21 +1,25 @@
 import { randomUUID } from "node:crypto";
+import { slug } from "./value-objects/slug";
 
 
 interface QuestionProps {
   title: string
   content: string
-   authorId: string
+  slug: slug
+  authorId: string
 }
 
 export class Question {
-  public title: string;
-  public content: string;
   public id: string;
+  public title: string;
+  public slug: slug;
+  public content: string;
   public authorId: string;
 
   constructor(props: QuestionProps, id?: string) {
     this.title = props.title;
     this.content = props.content;
+    this.slug = props.slug;
     this.authorId = props.authorId;
     this.id = id ?? randomUUID();
     
---
+++ typeScript/src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,11 @@
+export class slug {
+    public value: string
+
+    constructor(value: string) {
+        this.value = value
+    }
+
+    static createFromText(text: string) {
+        const slugText = text.normalize("NFKD").toLowerCase().trim().replace(/\s+/g, '').replace(/\[^\w]+/g, '').replace(/_/g, '')
+    }
+}
