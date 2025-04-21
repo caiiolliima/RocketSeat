5f1ea952c7b0affee8dda421ca5b9ef74c9c8aae
Caio Lima <lima.caioalves@gmail.com>
2025-04-21 20:08:38 -0300

    commit resolve bug in edit question and edit question spec

---
+++ typeScript/src/domain/forum/application/use-case/edit-question.ts
@@ -0,0 +1,48 @@
+import { Either, left, right } from "@/core/either";
+import { Question } from "../../enterprise/entities/question";
+import { QuestionRepository } from "../repositories/question-repositories";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { NotAllowedError } from "./errors/not-allowed-error";
+
+interface EditQuestionUseCaseRequest {
+  authorId: string;
+  questionId: string;
+  title: string;
+  content: string;
+}
+
+type EditQuestionUseCaseRequestResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {
+    question: Question;
+  }
+>;
+
+export class EditQuestionUseCase {
+  constructor(private questionRepository: QuestionRepository) {}
+
+  async execute({
+    authorId,
+    title,
+    content,
+    questionId,
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseRequestResponse> {
+    const question = await this.questionRepository.findById(questionId);
+
+    if (!question) {
+      return left(new ResourceNotFoundError());
+    }
+
+    if (authorId != question.authorId.toString()) {
+      return left(new NotAllowedError());
+    }
+
+    (question.title = title), (question.content = content);
+
+    await this.questionRepository.save(question);
+
+    return right({
+      question,
+    });
+  }
+}
