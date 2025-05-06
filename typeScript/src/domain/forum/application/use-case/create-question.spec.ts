e94f9c00f742e5ef2cfdaa0ca547f52489ce73e9
CaioLimaEK <caio.lima@elringklinger.com>
2025-05-06 16:08:05 -0300

    commit

--- typeScript/src/domain/forum/application/use-case/create-question.spec.ts
+++ typeScript/src/domain/forum/application/use-case/create-question.spec.ts
@@ -1,13 +1,17 @@
 import { InMemoryQuestionRepository } from "test/repositories/in-memory-questions-repository";
 import { CreateQuestionUseCase } from "./create-question";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
+import { InMemoryQuestionAttachmentRepository } from "test/repositories/in-memory-question-attachments-repository";
 
 let inMemoryQuestionRepository: InMemoryQuestionRepository;
+let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentRepository;
 let sut: CreateQuestionUseCase;
 
 describe("Create Question", () => {
   beforeEach(() => {
-    inMemoryQuestionRepository = new InMemoryQuestionRepository();
+    inMemoryQuestionAttachmentRepository =
+      new InMemoryQuestionAttachmentRepository();
+    inMemoryQuestionRepository = new InMemoryQuestionRepository(inMemoryQuestionAttachmentRepository);
     sut = new CreateQuestionUseCase(inMemoryQuestionRepository);
   });
 
@@ -21,8 +25,12 @@ describe("Create Question", () => {
 
     expect(result.isRight()).toBe(true);
     expect(inMemoryQuestionRepository.items[0]).toEqual(result.value?.question);
-    expect(inMemoryQuestionRepository.items[0].attachments.currentItems).toHaveLength(2);
-    expect(inMemoryQuestionRepository.items[0].attachments.currentItems).toEqual([
+    expect(
+      inMemoryQuestionRepository.items[0].attachments.currentItems
+    ).toHaveLength(2);
+    expect(
+      inMemoryQuestionRepository.items[0].attachments.currentItems
+    ).toEqual([
       expect.objectContaining({ attachmentId: new UniqueEntityId("1") }),
       expect.objectContaining({ attachmentId: new UniqueEntityId("2") }),
     ]);
