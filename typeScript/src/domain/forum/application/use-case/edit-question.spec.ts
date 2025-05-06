b1fee91997d9a072e44067fef6e15661ec3bf7f5
CaioLimaEK <caio.lima@elringklinger.com>
2025-05-06 14:02:03 -0300

    feat: edit question with attachment

--- typeScript/src/domain/forum/application/use-case/edit-question.spec.ts
+++ typeScript/src/domain/forum/application/use-case/edit-question.spec.ts
@@ -3,14 +3,22 @@ import { EditQuestionUseCase } from "./edit-question";
 import { InMemoryQuestionRepository } from "test/repositories/in-memory-questions-repository";
 import { UniqueEntityId } from "@/core/entities/unique-entity-id";
 import { NotAllowedError } from "./errors/not-allowed-error";
+import { InMemoryQuestionAttachmentRepository } from "test/repositories/in-memory-question-attachments-repository";
+import { makeQuestionAttachment } from "test/factories/make-question-attachment";
 
 let inMemoryQuestionRepository: InMemoryQuestionRepository;
+let inMemoryQuestionAttachmentRepository: InMemoryQuestionAttachmentRepository;
 let sut: EditQuestionUseCase;
 
 describe("Edit Question By Id", () => {
   beforeEach(() => {
     inMemoryQuestionRepository = new InMemoryQuestionRepository();
-    sut = new EditQuestionUseCase(inMemoryQuestionRepository);
+    inMemoryQuestionAttachmentRepository =
+      new InMemoryQuestionAttachmentRepository();
+    sut = new EditQuestionUseCase(
+      inMemoryQuestionRepository,
+      inMemoryQuestionAttachmentRepository
+    );
   });
 
   it("should be able to edit a question by id", async () => {
@@ -23,17 +31,37 @@ describe("Edit Question By Id", () => {
 
     await inMemoryQuestionRepository.create(newQuestion);
 
+    inMemoryQuestionAttachmentRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId("1"),
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityId("2"),
+      })
+    );
     await sut.execute({
       questionId: newQuestion.id.toValue(),
       authorId: "author-1",
       title: "Pergunta teste",
       content: "Conteudo teste",
+      attachmentIds: ["1", "3"],
     });
 
     expect(inMemoryQuestionRepository.items[0]).toMatchObject({
       title: "Pergunta teste",
       content: "Conteudo teste",
     });
+    expect(
+      inMemoryQuestionRepository.items[0].attachments.currentItems
+    ).toHaveLength(2);
+    expect(
+      inMemoryQuestionRepository.items[0].attachments.currentItems
+    ).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityId("1") }),
+      expect.objectContaining({ attachmentId: new UniqueEntityId("3") }),
+    ]);
   });
 
   it("should not be able to edit a question from another user", async () => {
@@ -51,6 +79,7 @@ describe("Edit Question By Id", () => {
       authorId: "author-2",
       title: "Pergunta teste",
       content: "Conteudo teste",
+      attachmentIds: [],
     });
     expect(result.isLeft()).toBe(true);
     expect(result.value).toBeInstanceOf(NotAllowedError);
---
+++ typeScript/test/repositories/in-memory-question-attachments-repository.ts
@@ -0,0 +1,16 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
+import { QuestionAttachmentRepository } from "@/domain/forum/application/repositories/question-attachment-repository";
+import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
+
+export class InMemoryQuestionAttachmentRepository
+  implements QuestionAttachmentRepository
+{
+  public items: QuestionAttachment[] = [];
+  async findManyQuestionId(questionId: string) {
+    const questionAttachments = this.items.filter(
+      (item) => item.questionId.toString() === questionId
+    );
+
+    return questionAttachments;
+  }
+}
