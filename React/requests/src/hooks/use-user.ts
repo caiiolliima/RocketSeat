a6a9aac7b150b60d151c7707d4146b7357e636bd
CaioLimaEK <Caio.Lima@elringklinger.com>
2025-07-22 09:58:17 -0300

    feat: create form user, create route api

--- React/requests/src/hooks/use-user.ts
+++ React/requests/src/hooks/use-user.ts
@@ -1,5 +1,5 @@
 import React from "react";
-import { fetcher } from "../helpers/api";
+import { api, fetcher } from "../helpers/api";
 import type { User } from "../models/user";
 
 export default function useUser() {
@@ -23,5 +23,20 @@ export default function useUser() {
     }
   }, []);
 
-  return { user, userRequestStatus: requestStatus, getUser };
+  async function createUser(payload: User) {
+    try {
+      setRequestStatus("saving");
+
+      await api("/users", { method: "POST", body: JSON.stringify(payload) });
+
+      alert("Usuário criado com sucesso");
+    } catch (error) {
+      console.error(error);
+      alert("Erro ao criar usuário");
+    } finally {
+      setRequestStatus("idle");
+    }
+  }
+
+  return { user, userRequestStatus: requestStatus, getUser, createUser };
 }
